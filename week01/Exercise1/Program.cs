8851bb8a98aad74a85b085435790eeb8fe03f716
Darlinton <barffourdarlinton@gmail.com>
2025-05-20 10:26:54 +0000

    Week01/Exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        //Promt the user for their first name
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+
+        //prompt the user for their last name
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+
+        // Print the user's name in the specified format
+        Console.WriteLine($"Your name is {last}, {first} {last}");
+
+          
     }
 }
